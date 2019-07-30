19d381fe8d4421ba7b3a48d37a5ec71f9670324f
Gabriel <pedromurillo96@gmail.com>
2019-07-30 10:05:58 -0700

    button attachment added and pausing prototype

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 var animes = ["One Piece", "Naruto", "One Punch Man", "My Hero Academia", "Bleach", "Fairy Tail", "Dragon Ball Z", "Food Wars!", "Fullmetal alchemist", "Soul eater", "Gintama", "Pokemon", "Hunter X hunter"];
 
-function pushData(){
-
+$(".click").on("click", function() {
     var inputText = document.getElementById("inputText").value;
-    
-    
     animes.push(inputText);
+    console.log(animes)
+    console.log("test")
+    $("#div1").empty();
+    createButtons();
+})
 
-    console.log(animes);
+// function pushData(){
+
+//     var inputText = document.getElementById("inputText").value;
     
+    
+//     animes.push(inputText);
+
+//     console.log(animes);
+//     $("#div1").empty();
+//     createButtons();
     
     
 
-}  
+// }  
+
+
 
 
 console.log(animes);
-pushData();
+
 function createButtons(){
     for (var i = 0; i < animes.length; i++) {
         var button = $("<button>");
@@ -38,7 +50,7 @@ createButtons();
  
 
 
-$(".animeButton").on("click", function(){
+$("#div1").on("click", ".animeButton", function(){
     console.log("a button was click");
     var anime = $(this).attr("data-anime");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -50,10 +62,36 @@ $(".animeButton").on("click", function(){
 
     .then(function(response){
         console.log(response);
-        for(var i = 0; i < response.data.length; i++){
-            var image = $("<img>");
-            image.attr("src", response.data[i].images.original.url)
-            $("#div3").append(image);
+        var results = response.data;
+        for(var i = 0; i < results.length; i++){
+            // Only taking action if the photo has an appropriate rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
+                
+                // Storing the result item's rating
+                var rating = results[i].rating;
+
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + rating);
+
+                // Creating an image tag
+                var image = $("<img>");
+
+                image.attr("data-still", results[i].images.original_still.url);
+
+                // Giving the image tag an src attribute of a proprty pulled off the
+                // result item
+                image.attr("src", results[i].images.original_still.url);
+
+                // Appending the paragraph and Image we created to the "#div3" div.
+                $("#div3").append(image);
+                $("#div3").append(p);
+
+
+
+
+
+
+            }
         }
     })
     
